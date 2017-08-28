634cb334ad34cc2fc1b8a2e7a90a1347069222af
Ayende Rahien <Ayende@ayende.com>
2017-08-28 12:32:07 +0300

    Initial support for arrow functions

---
+++ Jint.Tests/Runtime/ArrowFunctions.cs
@@ -0,0 +1,30 @@
+﻿using Xunit;
+
+namespace Jint.Tests.Runtime
+{
+    public class ArrowFunctions
+    {
+        [Fact]
+        public void CanParseFunctionCallingIt()
+        {
+            var engine = new Engine();
+            engine.Execute(@"
+var materials = [
+ 'Hydrogen',
+  'Helium',
+  'Lithium',
+  'Beryllium'
+];
+materials.map(function(material) {return  material.length;}); // [8, 6, 7, 9]
+
+materials.map(material => material.length); // [8, 6, 7, 9]
+");
+            var value = engine.GetCompletionValue();
+            Assert.Equal(4u, value.AsArray().GetLength());
+            Assert.Equal(8, value.AsArray().Get("0").AsNumber());
+            Assert.Equal(6, value.AsArray().Get("1").AsNumber());
+            Assert.Equal(7, value.AsArray().Get("2").AsNumber());
+            Assert.Equal(9, value.AsArray().Get("3").AsNumber());
+        }
+    }
+}
