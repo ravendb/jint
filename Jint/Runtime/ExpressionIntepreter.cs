b9da22198d3608fbcda8e7979a5a63c0863a01c6
Ayende Rahien <ayende@ayende.com>
2017-11-06 13:56:19 +0200

    Fixing usage of arrow function with statement bodies.

--- Jint/Runtime/ExpressionIntepreter.cs
+++ Jint/Runtime/ExpressionIntepreter.cs
@@ -787,23 +787,18 @@ namespace Jint.Runtime
 
         public JsValue EvaluateArrowFunctionExpression(ArrowFunctionExpression arrowFunctionExpression)
         {
-            if (arrowFunctionExpression.Expression)
-            {
-                var expression = arrowFunctionExpression.Body.As<Expression>();
-                var func = new FunctionExpression(new Identifier(null),
-                    arrowFunctionExpression.Params,
-                    new BlockStatement(new[] { new ReturnStatement(expression) }),
-                    false,
-                    new HoistingScope(),// TOOD: fix this
-                    StrictModeScope.IsStrictModeCode);
+            var statement =
+                arrowFunctionExpression.Expression
+                    ? new BlockStatement(new[] {new ReturnStatement(arrowFunctionExpression.Body.As<Expression>()) })
+                    : arrowFunctionExpression.Body.As<BlockStatement>();
+            var func = new FunctionExpression(new Identifier(null),
+                arrowFunctionExpression.Params,
+                statement,
+                false,
+                new HoistingScope(),// TOOD: fix this
+                StrictModeScope.IsStrictModeCode);
 
-                return EvaluateFunctionExpression(func);
-            }
-            var arrowFunc = new ClrFunctionInstance(_engine, (externalSelf, args) =>
-            {
-                return (JsValue)EvaluateExpression(arrowFunctionExpression.Body.As<Expression>());
-            });
-            return new JsValue(arrowFunc);
+            return EvaluateFunctionExpression(func);
         }
 
         public JsValue EvaluateCallExpression(CallExpression callExpression)
